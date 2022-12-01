f7ac96244cc12e03723f3b24a9009f9f3a6f3437
oliverLo78 <oliverberto@gmail.com>
2022-12-01 16:12:54 -0500

    added src-sw logic

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,81 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const WebpackPwaManifest = require('webpack-pwa-manifest');
+// const path = require('path');
+// const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// // TODO: Add and configure workbox plugins for a service worker and manifest file.
+// // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
+// module.exports = () => {
+//   return {
+//     mode: 'development',
+//     entry: {
+//       main: './src/js/index.js',
+//       install: './src/js/install.js'
+//     },
+//     output: {
+//       filename: '[name].bundle.js',
+//       path: path.resolve(__dirname, 'dist'),
+//     },
+//     plugins: [
       
-    ],
+//     ],
 
-    module: {
-      rules: [
+//     module: {
+//       rules: [
         
-      ],
-    },
-  };
+//       ],
+//     },
+//   };
+// };
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// TODO: Require the InjectManifest class of the WorkBoxPlugin 
+const {InjectManifest} = require('workbox-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Webpack Plugin',
+    }),
+    new MiniCssExtractPlugin(),
+   // TODO: Add InjectManifest Plugin
+   new InjectManifest({
+    swSrc: './src/sw.js',
+    swDest: 'service-worker.js'
+   })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
+
