475662885b804fde32c62c0ccab4a541377ee264
oliverLo78 <oliverberto@gmail.com>
2022-12-02 10:04:14 -0500

    updated wepackPwaManifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,81 +1,74 @@
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const WebpackPwaManifest = require('webpack-pwa-manifest');
-// const path = require('path');
-// const { InjectManifest } = require('workbox-webpack-plugin');
-
-// // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// // TODO: Add CSS loaders and babel to webpack.
-
-// module.exports = () => {
-//   return {
-//     mode: 'development',
-//     entry: {
-//       main: './src/js/index.js',
-//       install: './src/js/install.js'
-//     },
-//     output: {
-//       filename: '[name].bundle.js',
-//       path: path.resolve(__dirname, 'dist'),
-//     },
-//     plugins: [
-      
-//     ],
-
-//     module: {
-//       rules: [
-        
-//       ],
-//     },
-//   };
-// };
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// TODO: Require the InjectManifest class of the WorkBoxPlugin 
-const {InjectManifest} = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/js/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+module.exports = () => {
+  return {
+    mode: 'development',
+    // Entry point for files
+    entry: {
+      main: './src/js/index.js',
+    },
+    // Output for our bundles
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      // Webpack plugin that generates our html file and injects our bundles. 
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+     
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      title: 'Webpack Plugin',
-    }),
-    new MiniCssExtractPlugin(),
-   // TODO: Add InjectManifest Plugin
-   new InjectManifest({
-    swSrc: './src-sw.js',
-    swDest: 'service-worker.js'
-   })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'JATE',
+        short_name: 'jate',
+        description: 'just another text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
+
+    module: {
+      // CSS loaders
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
           },
         },
-      },
-    ],
-  },
+      ],
+    },
+  };
 };
-
